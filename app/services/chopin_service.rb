8d6f32dd6379976313551f6e9bd015f83019fe67
juan broz <f870421@gmail.com>
2017-10-14 12:44:10 -0500

    communication with bluemix fixed

---
+++ app/services/chopin_service.rb
@@ -0,0 +1,22 @@
+# app/services/chopin_service.rb
+
+class ChopinService
+
+  # We pass the image to bluemix and get the json back
+  def inspirate(picture)
+    json_partiture = { :categories => "tara!" }
+
+
+    json_partiture
+  end
+
+
+  # We pass the bluemix json
+  def compose(partiture)
+    # This should be doing the parsing and stuff
+
+
+    
+  end
+
+end
