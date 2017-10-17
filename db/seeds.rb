fd8b840fde248e2eafed22683ee252346521332e
juan broz <f870421@gmail.com>
2017-10-17 10:45:48 -0500

    basic testing stuff and improved the composition service

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,24 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-CategoryInformation.create(name: 'Person', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3')
-CategoryInformation.create(name: 'Animal', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3')
-CategoryInformation.create(name: 'Fruit', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3')
-CategoryInformation.create(name: 'Nothing', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3')
+# CategoryInformation.create(name: 'Person', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3')
+# CategoryInformation.create(name: 'Animal', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3')
+# CategoryInformation.create(name: 'Fruit', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3')
+# CategoryInformation.create(name: 'Nothing', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3')
+
+
+CategoryInformation.create(name: 'animal', url: 'http://satoshibutton.tk/Audire/animal.mp3')
+CategoryInformation.create(name: 'person', url: 'http://satoshibutton.tk/Audire/persona.mp3')
+CategoryInformation.create(name: 'man', url: 'http://satoshibutton.tk/Audire/hombre.mp3')
+CategoryInformation.create(name: 'woman', url: 'http://satoshibutton.tk/Audire/mujer.mp3')
+CategoryInformation.create(name: 'fruit', url: 'http://satoshibutton.tk/Audire/fruta.mp3')
+CategoryInformation.create(name: 'food', url: 'http://satoshibutton.tk/Audire/comida.mp3')
+CategoryInformation.create(name: 'cat', url: 'http://satoshibutton.tk/Audire/gato.mp3')
+CategoryInformation.create(name: 'dog', url: 'http://satoshibutton.tk/Audire/dog.mp3')
+CategoryInformation.create(name: 'weapon', url: 'http://satoshibutton.tk/Audire/arma.mp3')
+CategoryInformation.create(name: 'yellow color', url: 'http://satoshibutton.tk/Audire/amarillo.mp3')
+CategoryInformation.create(name: 'blue color', url: 'http://satoshibutton.tk/Audire/azul.mp3')
+CategoryInformation.create(name: 'green color', url: 'http://satoshibutton.tk/Audire/verde.mp3')
+CategoryInformation.create(name: 'red color', url: 'http://satoshibutton.tk/Audire/rojo.mp3')
+CategoryInformation.create(name: 'black color', url: 'http://satoshibutton.tk/Audire/negro.mp3')
+CategoryInformation.create(name: 'nothing', url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3')
